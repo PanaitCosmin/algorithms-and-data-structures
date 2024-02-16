743cdc3a0326f9c560a31f2b7dfcb81600c73535
PanaitCosmin <cosmindaniel.panait@gmail.com>
2024-02-17 00:36:24 +0200

    add grid traveler

---
+++ python/dynamic-programming/memoization/gridTraveler.py
@@ -0,0 +1,17 @@
+# Say that you are a traveler on a 2D grid.
+# You begin in the top-left corner and your goal is to travel to the bottom-rught corner.
+# You may only move down or right.
+# In how many ways can you travel to the goal on a grid with dimensions n * m ?
+
+def gridTraveler(n: int, m: int, memo = {}) -> int:
+        if (n, m) in memo:  return memo[(n, m)]
+        if m == 0 or n == 0:    return 0
+        if m == n == 1:    return 1
+
+        memo[(n, m)] = gridTraveler(n - 1, m, memo) + gridTraveler(n, m - 1, memo)
+
+        return memo[(n, m)]
+
+# testing
+print(gridTraveler(3,2)) # expected output: 3
+print(gridTraveler(3,3)) # expected output: 6
